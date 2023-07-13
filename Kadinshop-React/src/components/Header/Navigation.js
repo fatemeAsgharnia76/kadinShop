083aac34cfeb29357e3947a86febd6df58a4b06b
fatemeAsgharnia76 <139159540+fatemeAsgharnia76@users.noreply.github.com>
2023-07-13 12:43:16 +0330

    Update Navigation.js

--- Kadinshop-React/src/components/Header/Navigation.js
+++ Kadinshop-React/src/components/Header/Navigation.js
@@ -25,7 +25,6 @@ export default function Navigation(props) {
       }
       else {
           setsocialmediaicons(<SocialMediaIcons className={'mx-2'}/>)
-          console.log('cc')
       }
   }, [props.screenWidth,LogedIncontroler])
 
